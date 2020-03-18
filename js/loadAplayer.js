900ae1caa6304f8777ce1d2d8d18569527142d7f
SERNIKKI <badremilia@foxmail.com>
2020-03-19 03:47:38 +0800

    Site updated: 2020-03-19 03:47:35

--- js/loadAplayer.js
+++ js/loadAplayer.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         {
           name: "Bad Apple!!",
           artist: "のみこ / Masayoshi Minoshima",
-          url: "https://music.163.com/song/media/outer/url?id=22645196",
+          url: "https://m10.music.126.net/20200319034529/4397e44771251ea501dbc4e5675f30a8/ymusic/035e/5253/000c/445bb92590e95ffdd8f77fdfbadef708.mp3",
           cover: "https://i.loli.net/2020/03/18/BjFfxIwEWQ7dnHa.png"
         },
          {
@@ -23,19 +23,19 @@ document.addEventListener("DOMContentLoaded", function() {
            {
             name: '白石溪',
             artist: '乐正绫 / 純白P / 洛天依',
-            url: 'https://music.163.com/song/media/outer/url?id=415086297',
+            url: 'https://m10.music.126.net/20200319034649/c7f997afb01e664794b09c2a67f700ff/ymusic/fd19/51e1/15b4/60362d9754494dfa2f3a9cf131879906.mp3',
             cover: 'https://i.loli.net/2020/03/18/71RbqU98zgrDSOL.png',
           },
           {
             name: '谷雨',
             artist: '桑葚上的猴子',
-            url: 'https://music.163.com/song/media/outer/url?id=410161057',
+            url: 'https://m10.music.126.net/20200319034808/89ad615dea18e5400f25e324f4776c35/ymusic/7c82/e510/ff29/0468d404bb1341e483eec7af4e316d99.mp3',
             cover: 'https://i.loli.net/2020/03/18/CEIqzavcF6hkUdG.jpg',
           },
           {
             name: '樱花樱花想见你',
             artist: '南杉',
-            url: 'https://music.163.com/song/media/outer/url?id=438900492',
+            url: 'https://m10.music.126.net/20200319034856/547bc94db5083f1ec2ffcd81643dd3c8/ymusic/2b22/1a51/e3f2/511c5af259638c4c0b5ceaaf6fb1a128.mp3',
             cover: 'https://i.loli.net/2020/03/18/vaJQ1LsGehmEtRw.jpg',
           },
           {
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
           {
             name: '土著神の顶点 ~Native Faith',
             artist: 'Like a rabbit',
-            url: 'https://music.163.com/song/media/outer/url?id=4165168',
+            url: 'https://m10.music.126.net/20200319035005/1a749bf1f0ac0d45f2ecc36c016bc475/ymusic/59f1/5e48/623d/ac68cc6944a450cfda7f6d2b984838c0.mp3',
             cover: 'https://i.loli.net/2020/03/18/Ap51eaJx6V3BgfC.png',
           },
           {
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function() {
           {
             name: '横竖撇点折(泠鸢翻唱）（Cover：米白）',
             artist: '泠鸢yousa',
-            url: 'https://music.163.com/song/media/outer/url?id=1419023911',
+            url: 'https://m10.music.126.net/20200319035057/8c86a6c5377b632d3440b6d88905a880/ymusic/545f/0f08/0259/c5de9c2f728b2ff3e83a534aad01239e.mp3',
             cover: 'https://i.loli.net/2020/03/18/9hrDgt7bGJj8Koy.jpg',
           }
       ]
